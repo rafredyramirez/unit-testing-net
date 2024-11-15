bb436b4334c07799c083f65802c2860786b48b6e
rafredyramirez <ra.fredyramirez@gmail.com>
2024-11-15 09:16:09 -0500

    Se agrega a la solución un nuevo proyecto para el Test xUnit y se valida la primera prueba ConcatenaStrings

---
+++ StringManipulation.Tests/StringOperationTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation.Tests
+{
+    public class StringOperationTest
+    {
+        [Fact]
+        public void ConcatenateStrings()
+        {
+            var strOperations = new StringOperations();
+
+            var result = strOperations.ConcatenateStrings("Hello", "Word");
+
+            Assert.Equal("Hello Word", result);
+        }
+    }
+}
