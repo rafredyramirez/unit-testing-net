4e408b52e102a189edf1f45edbc561da4c691d3e
rafredyramirez <ra.fredyramirez@gmail.com>
2024-11-15 09:44:10 -0500

    Definición estructura AAA (Arrange, Act, Assert)

--- StringManipulation.Tests/StringOperationTest.cs
+++ StringManipulation.Tests/StringOperationTest.cs
@@ -11,10 +11,13 @@ namespace StringManipulation.Tests
         [Fact]
         public void ConcatenateStrings()
         {
+            // Arrange = Configurar todos los datos iniciales, datos de prueba, diferentes variantes u objetos que necesitan para la ejecución.
             var strOperations = new StringOperations();
 
+            // Act = Ejecución de la prueba perse que se desea probar
             var result = strOperations.ConcatenateStrings("Hello", "Word");
 
+            // Assert = Comprobación del resultado 
             Assert.Equal("Hello Word", result);
         }
     }
